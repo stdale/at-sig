5b2743c9309bc4541121b879a6719589e6fbefec
stewart <stewart.t.dale@gmail.com>
2019-12-08 23:53:00 -0800

    Update at-sig.ino
    completed all initial code and logic including:
    timer based and centered dection based auto signal turn off
    
    LEFT_OUT / RIGHT_OUT goes high when corresponding signal is active
    
    lots of good stuff!! Now to test

--- at-sig.ino
+++ at-sig.ino
@@ -19,23 +19,27 @@
 
 #define USE_SERIAL 1
 
-#define LEFT_INT         2
-#define RIGHT_INT        3
-#define DISP_CLK         4
-#define DISP_DIO         5
-#define INPUT_CLK        6
-#define INPUT_DT         7
-#define INPUT_SW         8
-#define CENTER_DECTOR    9
+#define LEFT_INT          2
+#define RIGHT_INT         3
+#define DISP_CLK          4
+#define DISP_DIO          5
+#define INPUT_CLK         6
+#define INPUT_DT          7
+#define INPUT_SW          8
+#define CENTER_DECTOR     9
+#define LEFT_OUT          10
+#define RIGHT_OUT         11
 
-#define EEPROM_KEY       0b11001100
-#define EEPROM_KEY_ADDY  0
-#define EEPROM_TIME_ADDY 1
-#define EEPROM_MODE_ADDY 2
+#define EEPROM_KEY        0b11001100
+#define EEPROM_KEY_ADDY   0
+#define EEPROM_TIME_ADDY  1
+#define EEPROM_MODE_ADDY  2
 
-#define MODE_TIME        0
-#define MODE_CENTER      1
-#define MODE_BOTH        2
+#define MODE_TIME         0
+#define MODE_CENTER       1
+#define MODE_BOTH         2
+
+#define CENTER_ALIGN_TIME 5000
 
 enum signalState {
   signalOff = 0,
@@ -63,6 +67,7 @@ int16_t last_write;
 
 int prog_mode;
 volatile unsigned long long time_tracking;
+volatile unsigned long long center_time_tracking;
 volatile centeredState center_tracking;
 volatile signalState currentLeftState;
 volatile signalState currentRightState;
@@ -72,9 +77,12 @@ void timerIsr() {
 }
 
 void leftISR() {
+  currentRightState = signalOff;
+  digitalWrite(RIGHT_OUT,LOW);
   currentLeftState = signalOn;
+  digitalWrite(LEFT_OUT,HIGH);
   if(prog_mode == MODE_TIME) {
-
+   time_tracking = millis();
   }else if(prog_mode == MODE_CENTER) {
     int startState = digitalRead(CENTER_DECTOR);
     switch(startState){
@@ -89,9 +97,12 @@ void leftISR() {
 }
 
 void rightISR() {  
+  currentLeftState = signalOff;
+  digitalWrite(LEFT_OUT,LOW);
   currentRightState = signalOn;
+  digitalWrite(RIGHT_OUT,HIGH);
   if(prog_mode == MODE_TIME) {
-
+   time_tracking = millis();
   }else if(prog_mode == MODE_CENTER) {
     int startState = digitalRead(CENTER_DECTOR);
     switch(startState){
@@ -193,16 +204,74 @@ void loop() {
         break;
     }
   }
-
-  if(prog_mode == MODE_TIME) {
-
-
-  }else if(prog_mode == MODE_CENTER) {
-
-
-  }else {
-
-
+  if(currentLeftState || currentRightState) {
+   if(prog_mode == MODE_TIME) {
+    if(time_tracking - millis() >= (value*1000)) {
+#ifdef USE_SERIAL
+       Serial.println("Reached end of turning timer: ");  
+#endif
+       if(currentRightState == signalOn) {
+#ifdef USE_SERIAL
+         Serial.println("Stopping Right signal");  
+#endif
+         currentRightState = signalOff;
+         digitalWrite(RIGHT_OUT,LOW);  
+       }else if(currentLeftState == signalOn){
+#ifdef USE_SERIAL
+         Serial.println("Stopping Left signal");  
+#endif
+         currentLeftState = signalOff;
+         digitalWrite(LEFT_OUT,LOW);
+       }
+    }
+   }else if(prog_mode == MODE_CENTER) {
+     int currentState = digitalRead(CENTER_DECTOR);
+     switch(center_tracking) {
+      case  startCentered:
+       if(currentState == LOW) {
+         center_tracking = unCentered;
+#ifdef USE_SERIAL
+         Serial.println("CENTERED: going from startCentered to unCentered");  
+#endif
+       }
+       break;
+      case unCentered:
+       if(currentState == HIGH) {
+        center_tracking = centered;
+        center_time_tracking = millis();
+#ifdef USE_SERIAL
+        Serial.println("CENTERED: going from unCentered to centered");  
+#endif
+       }
+       break;
+      case centered:
+       if(currentState == LOW) {
+         center_tracking = unCentered;
+#ifdef USE_SERIAL
+         Serial.println("CENTERED: going from centered to unCentered");  
+#endif
+       }else{
+         if(center_time_tracking - millis() >= (CENTER_ALIGN_TIME)) {
+#ifdef USE_SERIAL
+           Serial.println("CENTERED: centered for 5 seconds");  
+#endif
+           if(currentRightState == signalOn) {
+            currentRightState = signalOff;
+            digitalWrite(RIGHT_OUT,LOW);  
+#ifdef USE_SERIAL
+            Serial.println("CENTERED: turned off Right signal");  
+#endif
+           }else if(currentLeftState == signalOn){
+            currentLeftState = signalOff;
+            digitalWrite(LEFT_OUT,LOW);
+#ifdef USE_SERIAL
+            Serial.println("CENTERED: turned off Left signal");  
+#endif
+           }
+         }
+       }
+       break;
+    } 
+   }
   }
-  
 }
